8cd3ffc6c09ef094e4828dd6acabacb6fc7893ca
Marz Pimentel <marzmail@gmail.com>
2018-03-20 20:28:10 -0700

    added a shitty health UI. If the hearts were Batteries and better designed, it could look good.

---
+++ 01 Grimoire/Assets/Grim Scripts/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public Sprite[] BatterySprite;
+    public Image BatteryUI;
+    private Player player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update() {
+        BatteryUI.fillAmount = player.playerStats.Health * 0.1f;
+    }
+
+} 

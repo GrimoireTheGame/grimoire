77a81fb98bd45185fedc71fe02daa956ece7d9b7
Marz Pimentel <marzmail@gmail.com>
2018-03-22 16:29:06 -0700

    Added an Ammo UI counter. It works but there are bugs. It needs to reset the player and weapon variables whenever the player dies or when the player changes weapons.

--- 01 Grimoire/Assets/Grim Scripts/HUD.cs
+++ 01 Grimoire/Assets/Grim Scripts/HUD.cs
@@ -7,15 +7,25 @@ public class HUD : MonoBehaviour {
 
    
     public Image BatteryUI;
+    public Transform AmmoUI;
     private Player player;
+    private Weapon weapon;
 
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        weapon = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Weapon>();
     }
 
     void Update() {
+        //Works but has bugs
+        //The HUD will not work if the player dies, 
+        //and I'm guessign it won't work if the player changes weapons.
+            //The code needs to check each instance of the player and the weapon,
+            //because rn, it only checks at the beginning of the game 
+            //so if the player dies, it doesn't reset the player and weapon variables
         BatteryUI.fillAmount = player.playerStats.Health * 0.1f;
+        AmmoUI.GetComponent<Text>().text = weapon.ammunition.ToString() + " / " + weapon.maxAmmunition.ToString(); 
     }
 
 } 
